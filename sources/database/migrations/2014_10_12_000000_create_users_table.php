9068f52d840c19b7e4468fc8ab40a75fd5dbaa0b
Minh Tri <epsminhtri@gmail.com>
2024-02-13 23:53:26 +0700

    init

---
+++ sources/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    // code
+    // name
+    // nick_name
+    // birth_date
+    // gender
+    // phone
+    // address
+    // role
+    // date_register
+    // status
+    // create_at
+    // update_at
+    // delete_at
+    // email
+    // avt
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
